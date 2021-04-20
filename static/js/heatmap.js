c318266d90ab4b42352daab19d147a3aaed84854
chuckloveall <chuckloveall@gmail.com>
2021-04-19 21:55:42 -0700

    more fiddling with local file to attempt to solve cors error

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -12,9 +12,9 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(heatMap);
 
-const url = "https://opendata.arcgis.com/datasets/fd1d618ac3174ad5be730524a4dd778e_26.geojson";
-
-d3.json(url).then(response => {
+// const url = "https://opendata.arcgis.com/datasets/fd1d618ac3174ad5be730524a4dd778e_26.geojson";
+const geojson= "api/heatmapdata"
+d3.json(GeoData).then(response => {
 
   console.log(response);
 console.log(response.features[0].properties);
