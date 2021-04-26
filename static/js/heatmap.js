c05f3e9639ee50ffe29331d9fa93b8c467e91d46
LaurenGT <ltoothaker2@gmail.com>
2021-04-26 15:36:20 -0700

    adding tree designation bar chart. Minor CSS updates

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,6 +1,6 @@
 //=====================
 //start page load with map layer 
-const tile =  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+const tile = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
@@ -10,8 +10,8 @@ const tile =  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
   accessToken: API_KEY
 });
 const baseMaps = {
-   "Portland Map": tile
- };
+  "Portland Map": tile
+};
 // deprecated try to call using multiple files
 //const neighborhoods = {
 //     "Neighborhoods":
@@ -27,7 +27,7 @@ const baseMaps = {
 // },9500);
 // =====================
 // declare API url links for promise
-const census= "/api/censusdata/"
+const census = "/api/censusdata/"
 const neighborhoodURL = "https://opendata.arcgis.com/datasets/9f50a605cf4945259b983fa35c993fe9_125.geojson"
 const treesURL = "https://opendata.arcgis.com/datasets/fd1d618ac3174ad5be730524a4dd778e_26.geojson"
 
@@ -35,67 +35,67 @@ const treesURL = "https://opendata.arcgis.com/datasets/fd1d618ac3174ad5be730524a
 // const neigh=
 Promise.all([d3.json(neighborhoodURL), d3.json(treesURL), d3.json(census)]).then(([neighborhoods, trees, ucb]) => {
 
-console.log(neighborhoods)
-console.log(trees)
-console.log(ucb);
-console.log(trees.features[0].properties);
+  console.log(neighborhoods)
+  console.log(trees)
+  console.log(ucb);
+  console.log(trees.features[0].properties);
 
-const markers= L.markerClusterGroup();
-for (let i = 0; i < trees.features.length; i++) {
-  const location = trees.features[i].properties;
-  // console.log(location)
-  if (location.LAT) {
-    markers.addLayer(L.marker([location.LAT, location.LON])
-    .bindPopup(`<br><strong>Year Designated: </strong>${trees.features[i].properties.YEAR_Designated}</br>\
+  const markers = L.markerClusterGroup();
+  for (let i = 0; i < trees.features.length; i++) {
+    const location = trees.features[i].properties;
+    // console.log(location)
+    if (location.LAT) {
+      markers.addLayer(L.marker([location.LAT, location.LON])
+        .bindPopup(`<br><strong>Year Designated: </strong>${trees.features[i].properties.YEAR_Designated}</br>\
         <br><strong>Spread: </strong>${trees.features[i].properties.SPREAD} feet</br>\
         <br><strong>Diameter: </strong>${trees.features[i].properties.DIAMETER} feet</br>\
         <br><strong>Height: </strong> ${trees.features[i].properties.HEIGHT} feet</br>\
         <br><strong>Details: </strong>${trees.features[i].properties.NOTES}</br>`));
+    }
   }
-}
-// console.log(location);
-// console.log(heatArray);
-map.addLayer(markers)
- L.geoJson(neighborhoods, {
-  style: function(feature) {
-    return{
-      color: "white",
-      fillColor:"lightblue",
-      fillOpacity: 0.5,
-      weight: 1.5
-    };
-  },
-  onEachFeature: function(feature, layer) {
-    layer.on({
-      mouseover: function(event) {
-        layer = event.target;
-        layer.setStyle({
-          fillOpacity:0.9
-        });
-      },
-      mouseout: function(event) {
-        layer = event.target;
-        layer.setStyle({
-          fillOpacity: 0.5
-        });
-      },
-      click: function(event) {
-        map.fitBounds(event.target.getBounds());
-      }
-    });
-  layer.bindPopup(
-    `<h4>${feature.properties.NAME}</h4>`
-  );
-  },
+  // console.log(location);
+  // console.log(heatArray);
+  map.addLayer(markers)
+  L.geoJson(neighborhoods, {
+    style: function (feature) {
+      return {
+        color: "white",
+        fillColor: "lightblue",
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    },
+    onEachFeature: function (feature, layer) {
+      layer.on({
+        mouseover: function (event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        mouseout: function (event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.5
+          });
+        },
+        click: function (event) {
+          map.fitBounds(event.target.getBounds());
+        }
+      });
+      layer.bindPopup(
+        `<h4>${feature.properties.NAME}</h4>`
+      );
+    },
   }
-).addTo(map)
-// .addTo(map)
+  ).addTo(map)
+  // .addTo(map)
 });
 
-const map= L.map("map", {
- center: [45.5051, -122.6754],
- zoom: 12,
- layers: tile
+const map = L.map("map", {
+  center: [45.5051, -122.6754],
+  zoom: 12,
+  layers: tile
 });
 
 // const neighbors = {
--- static/js/treedesignations.js
+++ static/js/treedesignations.js
@@ -1,80 +1,81 @@
 const heritagetrees = "https://opendata.arcgis.com/datasets/fd1d618ac3174ad5be730524a4dd778e_26.geojson"
 
-d3.json(heritagetrees).then(data => {
-    console.log(data.features)
-})
+d3.json(heritagetrees).then(trees => {
+    console.log(trees)
+    console.log(trees.features)
 
-Highcharts.chart('chart1', {
+    let yearDesignated = []
 
-    title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
-    },
+    for (let i = 0; i < trees.features.length; i++) {
+        const designation = trees.features[i].properties.YEAR_Designated;
+        // console.log(designation)
 
-    subtitle: {
-        text: 'Source: thesolarfoundation.com'
-    },
-
-    yAxis: {
-        title: {
-            text: 'Number of Employees'
+        if (designation) {
+            yearDesignated.push(designation)
         }
-    },
-
-    xAxis: {
-        accessibility: {
-            rangeDescription: 'Range: 2010 to 2017'
-        }
-    },
-
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
-    },
-
-    plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
-            },
-            pointStart: 2010
-        }
-    },
-
-    series: [{
-        name: 'Installation',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-    }, {
-        name: 'Manufacturing',
-        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-    }, {
-        name: 'Sales & Distribution',
-        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-    }, {
-        name: 'Project Development',
-        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-    }, {
-        name: 'Other',
-        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-    }],
-
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
-                },
-                
-            }
-        }]
     }
+    console.log(yearDesignated)
 
+    let sortedYearDes = yearDesignated.sort((a, b) => a - b);
+    // console.log(sortedYearDes)
+    let counts = {};
+
+    for (var i = 0; i < sortedYearDes.length; i++) {
+        var num = sortedYearDes[i];
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+    //   console.log(counts);
+
+    let designationYear = Object.keys(counts);
+    let treeCount = Object.values(counts);
+
+    console.log(designationYear);
+    console.log(treeCount);
+
+    Highcharts.chart('chart1', {
+
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Heritage Trees Designated by Year'
+        },
+        subtitle: {
+            text: 'Source: Portland Open Data'
+        },
+        xAxis: {
+            categories: designationYear,
+            crosshair: true,
+            title: {
+                text: "Year Designated"
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Number of Trees Designated'
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                return '<strong>' + this.x +
+                    '</strong>: <p>' + this.y + '</p>';
+            }
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            showInLegend: false,
+            data: treeCount
+        }]
+    });
 });
+
+
 
 
 // copy of above for formatting
