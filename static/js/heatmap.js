b3519acb44b4af0efae8a85aa47df9aa2ba1791e
chuckloveall <chuckloveall@gmail.com>
2021-04-26 21:23:34 -0700

    stash ignore

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,5 +1,5 @@
 //=====================
-//start page load with map layer 
+//start page load with map layer
 const tile = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -356,8 +356,6 @@ Highcharts.chart('chart4', {
       data: treeCount
     }]
   });
-
-});
 
 const map = L.map("map", {
   center: [45.5051, -122.6754],
